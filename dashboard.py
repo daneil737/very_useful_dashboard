ad87c393181f4139cb9b4e9621c978cfd55f31a0
Daniel Karczewicz <dkarczewicz.2000@gmail.com>
2026-08-17 21:04:08 +0100

    last and next Pogon Szczecin matches funtionality was added

--- dashboard.py
+++ dashboard.py
@@ -5,13 +5,12 @@ import os
 
 GOAL_API_KEY = os.environ["GOAL_API_KEY"]
 GEOCODE_API_KEY = os.environ["GEOCODE_API_KEY"]
-
+LAT = 53.5484398
+LONG = -2.522554
 
 app = Flask(__name__)
 
 def get_weather():
-    LAT = "53.5484398"
-    LONG = "-2.522554"
     api_call_str = f"https://api.open-meteo.com/v1/forecast?latitude={LAT}&longitude={LONG}&current=temperature_2m,rain,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms"
     weather_request = requests.get(api_call_str)
     temperature = weather_request.json()["current"]["temperature_2m"]
@@ -31,11 +30,36 @@ def get_weather():
 
 
 def get_city():
-    LAT = 53.5484398
-    LONG = -2.522554
     api_call_str = f"http://api.openweathermap.org/geo/1.0/reverse?lat={LAT}&lon={LONG}&limit=2&appid={GEOCODE_API_KEY}"
     geocode_request = requests.get(api_call_str)
     return (geocode_request.json()[0]["name"], geocode_request.json()[0]["state"])
+
+
+def call_goal_api():
+    headers = {
+        "Authorization" : GOAL_API_KEY
+    }
+    url = f"https://api.goal-api.com/v1/teams/cmri4pc28d2lclb07pr43de75/fixtures"
+    response = requests.get(url, headers=headers)
+    return response.json()['data']
+
+
+def get_next_match(api_response):
+    next_match = [match for match in api_response if match['matchStatus'] == "SCHEDULED"][-1]
+    return {"date": next_match['matchDate'],
+            "time": next_match['matchTime'],
+            "home_team": next_match['homeTeam']['name'],
+            "away_team": next_match['awayTeam']['name']}
+
+
+def get_last_match(api_response):
+    last_match = [match for match in api_response if match['matchStatus'] == "FINISHED"][0]
+    return {"date": last_match['matchDate'],
+            "time": last_match['matchTime'],
+            "home_team": last_match['homeTeam']['name'],
+            "away_team": last_match['awayTeam']['name'],
+            "score": f"{last_match['homeTeamFtScore']}:{last_match['awayTeamFtScore']}"}
+
 
 
 @app.route("/")
@@ -51,5 +75,14 @@ def mainpage():
         "wind": api_wind,
         "wind_direction": api_wind_dir
     }
+
+
+    goal_api_response = call_goal_api()
+    api_last_match_data = get_last_match(goal_api_response)
+    api_next_match_data = get_next_match(goal_api_response)
+
     
-    return render_template("index.html", weather_data=api_weather_data)
+    return render_template("index.html",
+                           weather_data=api_weather_data,
+                           last_match_data=api_last_match_data,
+                           next_match_data=api_next_match_data)
